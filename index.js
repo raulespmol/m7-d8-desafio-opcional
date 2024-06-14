5cfd8febe8a4ebc5d4cdb19a135c236d6f992f7a
Raul Espinoza M <raul.espmol@gmail.com>
2024-06-14 19:28:13 -0400

    validar credenciales

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ app.use(express.json())
 
 const { obtenerJugadores, registrarJugador } = require('./src/controllers/jugadores')
 const { obtenerEquipos, agregarEquipo } = require('./src/controllers/equipos')
+const { iniciarSesion } = require('./src/controllers/login')
 
+app.post("/login", iniciarSesion)
 
 app.get("/equipos", obtenerEquipos)
 app.post("/equipos", agregarEquipo)
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,18 @@
-const secretKey = "ULTRA SECRET KEY :O"
+require('dotenv').config()
 
-module.exports = { secretKey }
+const secretKey = process.env.SECRET
+
+/* 
+Absolutamente este no es el metodo para validar credenciales
+pero lo haré de esta forma para fines practicos de este desafio
+y no tener que insertar las credenciales de un usuario en la Base de Datos
+*/
+const verificarCredenciales = (credenciales) => {
+  const {username, password} = credenciales
+  return username === "admin" && password === "1234"
+}
+
+module.exports = { 
+  secretKey,
+  verificarCredenciales
+}
