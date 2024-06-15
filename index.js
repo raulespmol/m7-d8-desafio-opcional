00ae2807aa92362b2f8a7e26011d7cc7810d631a
Raul Espinoza M <raul.espmol@gmail.com>
2024-06-15 11:31:55 -0400

    middleware validacion token

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ app.use(express.json())
 const { obtenerJugadores, registrarJugador } = require('./src/controllers/jugadores')
 const { obtenerEquipos, agregarEquipo } = require('./src/controllers/equipos')
 const { iniciarSesion } = require('./src/controllers/login')
+const { validarToken } = require('./src/middlewares/autorizacion')
 
 app.post("/login", iniciarSesion)
 
 app.get("/equipos", obtenerEquipos)
-app.post("/equipos", agregarEquipo)
+app.post("/equipos", validarToken, agregarEquipo)
 
 app.get("/equipos/:teamID/jugadores", obtenerJugadores)
-app.post("/equipos/:teamID/jugadores", registrarJugador)
+app.post("/equipos/:teamID/jugadores", validarToken, registrarJugador)
